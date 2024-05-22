3d6ba79f3d53e47d6a4079431c0df3cb0ebfa2d5
hjin111 <https://github.com/hjin111>
2024-05-22 17:40:55 +0900

    day7

--- 11.dump_practice.sh
+++ 11.dump_practice.sh
@@ -4,3 +4,20 @@ mysqldump -u root -p --default-character-set=utf8mb4 board > dumpfile.sql
 mysqldump -uroot -p board -r dumpfile.sql 
 
 # dump파일을 github에 업로드
+
+# 리눅스에서 mariadb 서버 설치
+sudo apt-get install mariadb-server
+
+# mariadb 서버 시작
+sudo systemctl start mariadb
+
+# mariadb 접속 테스트
+sudo mariadb -u root -p
+
+# git 설치
+sudo apt install git
+
+# git을 통해 repo clone
+git clone 레파지토리주소
+cd mariadb_syntax # mariadb_syntax로 들어가기
+mysqldump -u root -p board < dumpfile.sql
